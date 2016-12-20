5f5aee9af0b3c123531ad43ae8caf896c42f6285
sisobus <sisobus1@gmail.com>
2016-12-20 11:36:38 +0900

    add all

---
+++ goodsoup/config.py
@@ -0,0 +1,19 @@
+__author__ = 'sisobus'
+UPLOAD_FOLDER = '/var/www/goodsoup/goodsoup/static/files'
+GS_DATABASE_URI   = 'mysql+pymysql://root:tkdrmsld123@222.239.250.23/goodsoup'
+GS_SECRET_KEY     = 'SET T0 4NY SECRET KEY L1KE RAND0M H4SH'
+
+GS_RESIZE_URL       = 'http://www.goodsoup.co.kr'
+GS_RESIZE_ROOT      = '/var/www/goodsoup/goodsoup'
+GS_RESIZE_CACHE_DIR = 'static/files/cache'
+
+GS_IMP_TEST_KEY         = 'imp27079569'
+GS_IMP_TEST_API_KEY         = '5100335349417378'
+GS_IMP_TEST_SECRET_KEY  = 'uWBTTBxtKyu6U2sj0VtKhbzAVCmU8ZQCVswO33iNCNUwQnPRijBAvbaqNxkWwbzsD5hG9AuLooieL854'
+
+MAIL_SERVER     = 'smtp.googlemail.com'
+MAIL_PORT       = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'ko.goodsoup@gmail.com'
+MAIL_PASSWORD = 'tkdrmsld123'
